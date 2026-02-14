abb55f04e1c39fa1c6bb252ec2d21784c2664e67
MGalvezLanders <galvezlandersmaximo@gmail.com>
2026-02-14 18:25:27 -0300

    funcion de poner modo oscuro boton de modo oscuro y estilo al boton

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ const boton = document.getElementById('miBoton');
 boton.addEventListener('click', function() {
     console.log('Apretratste el boton');
 })
+
+const modoOscuro = document.getElementById('modoOscuro');
+modoOscuro.addEventListener('click', function(event){
+    event.target.style.backgroundColor = 'black';
+    event.target.style.color = 'white';
+    event.tarjet.textContent = 'Modo Claro';
+    event.tarjet.id = 'modoClaro';
+})
