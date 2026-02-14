4610f83f1fe335f7415e725b3c3d1fa4fa8be599
MGalvezLanders <galvezlandersmaximo@gmail.com>
2026-02-14 20:07:04 -0300

    cree funcion para cambiar el color de la letra si el fondo es oscuro

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@ function generarColorHex() { //genera el color hex
     const numero = Math.floor(Math.random() * 16777215);
     return "#" + numero.toString(16).padStart(6, "0");
 }
+function esColorOscuro(hex) {
+  const r = parseInt(hex.substring(1, 3), 16);
+  const g = parseInt(hex.substring(3, 5), 16);
+  const b = parseInt(hex.substring(5, 7), 16);
+
+  const luminosidad = 0.299 * r + 0.587 * g + 0.114 * b;
+
+  return luminosidad < 128;
+}
 
 function generarPaleta(cantidad) {
 
@@ -18,12 +27,19 @@ function generarPaleta(cantidad) {
     section.style.backgroundColor = color;
     section.textContent = color;
 
+    if (esColorOscuro(color)) {
+        section.style.color = "white";
+    } else {
+        section.style.color = "black";
+    }
+
+
     contenedor.appendChild(section);
   }
 }
 
 boton.addEventListener("click", () => {
-  generarPaleta(5);
+  generarPaleta(6);
 });
 
 
