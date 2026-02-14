1e26241d5a3c73b311d5e63d0a1f11723d5fc952
MGalvezLanders <galvezlandersmaximo@gmail.com>
2026-02-14 19:44:21 -0300

    cree el css para crear una paleta y cree el css y el html lo simplifique

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 const boton = document.getElementById('miBoton');
-boton.addEventListener('click', function() {
-    console.log('Apretratste el boton');
-})
+const contenedor = document.getElementById('paleta');
 
-const modoOscuro = document.getElementById('modoOscuro');
-modoOscuro.addEventListener('click', function(event){
-    event.target.style.backgroundColor = 'black';
-    event.target.style.color = 'white';
-    event.tarjet.textContent = 'Modo Claro';
-    event.tarjet.id = 'modoClaro';
-})
+function generarColorHex() { //genera el color hex
+    const numero = Math.floor(Math.random() * 16777215);
+    return "#" + numero.toString(16).padStart(6, "0");
+}
+
+function generarPaleta(cantidad) {
+
+  contenedor.innerHTML = ""; //limpia la paleta anterior
+
+  for (let i = 0; i < cantidad; i++) {
+    const color = generarColorHex();
+
+    const section = document.createElement("section");
+    section.classList.add("color-box");
+    section.style.backgroundColor = color;
+    section.textContent = color;
+
+    contenedor.appendChild(section);
+  }
+}
+
+boton.addEventListener("click", () => {
+  generarPaleta(5);
+});
+
+
+
+
